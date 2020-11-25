45ad1b9d2654c4f855516b5721dd04b523c7b27a
dinaAlsaid <dinaAlsaid@github.com>
2020-11-26 00:03:31 +0200

    seriously is that it?

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ToDo from './components/todo/todo-connected.js';
+import Login from './components/todo/login';
+import LoginProvider from './context/login.js';
 import SettingsProvider from './context/settings.js';
 import PagesProvider from './context/pages.js';
 
@@ -8,11 +10,14 @@ import PagesProvider from './context/pages.js';
 export default function App() {
   return (
     <>
-    <SettingsProvider>
-      <PagesProvider>
-      <ToDo />
-      </PagesProvider>
-    </SettingsProvider>
+    <LoginProvider>
+      <Login />
+      <SettingsProvider>
+        <PagesProvider>
+        <ToDo />
+        </PagesProvider>
+      </SettingsProvider>
+    </LoginProvider>
     </>
   );
 

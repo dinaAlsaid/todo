b230931d7c72003aaf89f2e0dcfaaf30172b4eb9
dinaAlsaid <dinamalsaid@gmail.com>
2022-06-13 16:18:02 +0300

    react hook form

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import useFormHandler from "../../hooks/form.js";
+import { Row, Col, Button } from "react-bootstrap";
+import { HiCheck, HiX } from "react-icons/all";
+import { useForm } from "react-hook-form";
+
+// import useFormHandler from "../../hooks/form.js";
 import { HandwrittenItem } from "../designElements/HandwrittenItem/index.js";
 import { TodoListItem } from "../designElements/ListItem/index";
-
-import { HiCheck, HiX } from "react-icons/all";
-import { Row, Col } from "react-bootstrap";
 
 function TodoForm(props) {
   return (
@@ -31,25 +31,38 @@ function TodoForm(props) {
 export default TodoForm;
 
 const AddItemForm = (props) => {
-  const [handleInputChange, handleSubmit] = useFormHandler();
+  // const [handleInputChange, handleSubmit] = useFormHandler();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   return (
-    <form onSubmit={(e) => handleSubmit(e, props.handleSubmit)}>
+    <form onSubmit={handleSubmit(props.handleSubmit)}>
       <Row>
         <Col md="auto">
-          <input type="text" autoComplete="off" name="text" placeholder="Add New Item" onChange={handleInputChange} />
+          <Row>
+            <input
+              type="text"
+              autoComplete="off"
+              name="text"
+              placeholder="Add New Item"
+              {...register("text", { required: { value: true, message: "what is the task???" } })}
+            />
+          </Row>
+
+          <Row>
+            <span className="text-danger lh-sm">{errors.text?.message}</span>
+          </Row>
         </Col>
         <Col md="auto">
-          <input
-            type="text"
-            autoComplete="off"
-            name="assignee"
-            placeholder="Assigned To"
-            onChange={handleInputChange}
-          />
+          <input type="text" autoComplete="off" name="assignee" placeholder="Assigned To" {...register("assignee")} />
+          <span>{errors.assignee?.message}</span>
         </Col>
         <Col md="auto">
-          <input type="range" defaultValue="1" min="1" max="5" name="importance" onChange={handleInputChange} />
+          <input type="range" defaultValue="1" min="1" max="5" name="importance" {...register("importance")} />
+          <span>{errors.importance?.message}</span>
         </Col>
         <Col>
           <Button variant="link" className="button-icon" type="submit">

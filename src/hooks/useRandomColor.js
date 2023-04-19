538b2417c97f3a31808c61b656e8d491f4c0538f
dinaAlsaid <dinamalsaid@gmail.com>
2023-04-19 10:23:44 +0300

    remove warning

--- src/hooks/useRandomColor.js
+++ src/hooks/useRandomColor.js
@@ -4,5 +4,6 @@ import { randomColorGenerator } from "Util/randomColorGenerator";
 export const useRandomColor = () => {
   const [tabColor, setTabColor] = useState(randomColorGenerator());
 
+  console.log(setTabColor)
   return tabColor;
 };
